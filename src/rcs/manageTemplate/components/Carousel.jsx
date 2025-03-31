eaa81553a2f65e99671e2f31458008efa10aed19
Arihant Jain <arihantj916@gmail.com>
2025-03-31 12:05:33 +0530

    feat: render card content based on selectedCardIndex

--- src/rcs/manageTemplate/components/Carousel.jsx
+++ src/rcs/manageTemplate/components/Carousel.jsx
@@ -3,6 +3,7 @@ import InputField from "@/whatsapp/components/InputField";
 import { Variables } from "./Variables";
 import { useEffect, useState } from "react";
 import { SuggestedActions } from "./SuggestedActions";
+import toast from "react-hot-toast";
 
 export const Carousel = ({
   caraousalData,
@@ -21,17 +22,65 @@ export const Carousel = ({
   const [variables, setVariables] = useState([]);
   const [messageContent, setMessageContent] = useState("");
   const [btnData, setBtnData] = useState([]);
+  const [selectedCardIndex, setSelectedCardIndex] = useState(0);
 
   // useEffect(() => {
   //   console.log("Var", variables);
   //   console.log("Message", messageContent);
   // }, [messageContent, variables]);
+
+  useEffect(() => {
+    if (caraousalData.length == 0) {
+      setCaraousalData([
+        {
+          cardTitle: "Sample Card1 Title",
+          cardDescription: "Sample Card1 Description",
+          fileName: "C://Users//hp//Desktop//New folder//1.jpeg",
+          suggestions: [],
+        },
+      ]);
+    }
+  }, [caraousalData]);
+  // useEffect(() => {
+  //   // if (caraousalData.length > 0) {
+  //   //   setCaraousalData([
+  //   //     {
+  //   //       cardTitle: "Sample Card1 Title",
+  //   //       cardDescription: "Sample Card1 Description",
+  //   //       fileName: "C://Users//hp//Desktop//New folder//1.jpeg",
+  //   //       suggestions: [],
+  //   //     },
+  //   //   ]);
+  //   // }
+  //   console.log(caraousalData);
+  // }, [caraousalData]);
+
+  function handleSelectedCardIndex() {
+    if (selectedCardIndex === 9) {
+      toast.error("You cannot add more than 10 cards.");
+      return;
+    }
+    console.log(selectedCardIndex + 1);
+    setSelectedCardIndex((prev) => prev + 1);
+  }
+
   return (
     <div className="flex flex-col gap-2">
+      <button
+        className="border border-gray-400 rounded-sm h-8.5 text-sm bg-gray-200 px-2"
+        onClick={() => {
+          setCaraousalData([...caraousalData, {}]);
+          handleSelectedCardIndex();
+        }}
+        type="button"
+      >
+        Add Card
+      </button>
+
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         <AnimatedDropdown
           id={"selectCardWidth"}
-          label="Select Card Width"
+          label={`Select Card Width ${selectedCardIndex + 1}`}
           name={"selectCardWidth"}
           options={[
             {
@@ -46,12 +95,12 @@ export const Carousel = ({
           placeholder="Select Card Width"
           value={cardwidth}
           onChange={(e) => {
-            setcardwidth(e);
+            setCardwidth(e);
           }}
         />
         <AnimatedDropdown
           id={"selectCardHeight"}
-          label="Select Card Height"
+          label={`Select Card Height ${selectedCardIndex + 1}`}
           name={"selectCardHeight"}
           options={[
             {
@@ -82,6 +131,9 @@ export const Carousel = ({
         setBtnData={setBtnData}
         btnData={btnData}
       />
+      {/* 
+      {JSON.stringify(caraousalData, null, 2)}
+      {JSON.stringify(cardwidth, null, 2)} */}
     </div>
   );
 };
