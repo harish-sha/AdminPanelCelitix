f1e6f76ed5dd4555e734b340fe3733ac6e0e3181
Arihant Jain <arihantj916@gmail.com>
2025-03-31 10:39:11 +0530

    feat: add dropdown for cardHeight and CardWidth in Carousel Component

--- src/rcs/manageTemplate/components/Carousel.jsx
+++ src/rcs/manageTemplate/components/Carousel.jsx
@@ -1,3 +1,58 @@
-export const Carousel = ({ caraousalData, setCaraousalData }) => {
-  return <h1>Caraousal</h1>;
+import AnimatedDropdown from "@/whatsapp/components/AnimatedDropdown";
+import InputField from "@/whatsapp/components/InputField";
+
+export const Carousel = ({
+  caraousalData,
+  setCaraousalData,
+  cardwidth,
+  setCardwidth,
+  cardheight,
+  setCardheight,
+}) => {
+  return (
+    <div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <AnimatedDropdown
+          id={"selectCardWidth"}
+          label="Select Card Width"
+          name={"selectCardWidth"}
+          options={[
+            {
+              label: "Small",
+              value: "small",
+            },
+            {
+              label: "Medium",
+              value: "medium",
+            },
+          ]}
+          placeholder="Select Card Width"
+          value={cardwidth}
+          onChange={(e) => {
+            setcardwidth(e);
+          }}
+        />
+        <AnimatedDropdown
+          id={"selectCardHeight"}
+          label="Select Card Height"
+          name={"selectCardHeight"}
+          options={[
+            {
+              label: "Small",
+              value: "small",
+            },
+            {
+              label: "Medium",
+              value: "medium",
+            },
+          ]}
+          placeholder="Select Card Height"
+          value={cardheight}
+          onChange={(e) => {
+            setCardheight(e);
+          }}
+        />
+      </div>
+    </div>
+  );
 };
