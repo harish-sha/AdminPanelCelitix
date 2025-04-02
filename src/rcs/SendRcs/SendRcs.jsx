fb4a4117c2e6a03d6b7f77156faecb8b2aa6bb50
Arihant Jain <arihantj916@gmail.com>
2025-04-02 11:25:03 +0530

    feat: make component to handle sendRcsCampaign

--- src/rcs/SendRcs/SendRcs.jsx
+++ src/rcs/SendRcs/SendRcs.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import AnimatedDropdown from "@/whatsapp/components/AnimatedDropdown";
+import InputField from "@/whatsapp/components/InputField";
+import { HandleCampaignDetails } from "./components/CampaignInputDetails";
 
 const SendRcs = () => {
   return (
-    <div>
-      SendRcs
+    <div className="grid grid-cols-1 gap-2 md:grid-cols-3 ">
+      <div className="border border-red-500 min-h-[80vh] p-2">
+        <HandleCampaignDetails />
+      </div>
+      <div className="border border-purple-500">2</div>
+      <div className="border border-blue-500">3</div>
     </div>
-  )
-}
+  );
+};
 
-export default SendRcs
+export default SendRcs;
---
+++ src/rcs/SendRcs/components/CampaignInputDetails.jsx
@@ -0,0 +1,78 @@
+import AnimatedDropdown from "@/whatsapp/components/AnimatedDropdown";
+import InputField from "@/whatsapp/components/InputField";
+
+export const HandleCampaignDetails = () => {
+  return (
+    <div className="w-full p-3 bg-gray-100 rounded-lg shadow-md lg:flex-1">
+      <div className="mb-3">
+        <AnimatedDropdown
+          id="selectAgent"
+          name="selectAgent"
+          label="Select Agent"
+          tooltipContent="Select your Agent "
+          tooltipPlacement="right"
+          options={[{ value: "option1", label: "Option 1" }]}
+          value={""}
+          onChange={() => {}}
+          placeholder="Select Agent"
+        />
+      </div>
+
+      <div className="mb-3">
+        <InputField
+          id="createCampaign"
+          name="createCampaign"
+          label="Campaign Name"
+          value={"inputValue"}
+          onChange={(e) => {}}
+          placeholder="Campaign Name"
+          tooltipContent="Your templatename should not contain spaces."
+          tooltipPlacement="right"
+        />
+      </div>
+      <div className="mb-3">
+        <AnimatedDropdown
+          id="selectRcsTemplate"
+          name="selectRcsTemplate"
+          label="Select RCS Template"
+          tooltipContent="Select RCS Template"
+          tooltipPlacement="right"
+          options={[{ value: "text", label: "Text" }]}
+          value={""}
+          onChange={(value) => {
+            // setSelectedTemplate(value);
+            // setTemplateDataNew(null);
+            // setFormData({});
+            // setImagePreview(null);
+            // setImageFile(null);
+            // setFileHeaders([]);
+          }}
+          placeholder="Select Template"
+        />
+      </div>
+      {/* <div>
+                            {isFetching ? (
+                              <UniversalSkeleton height="15rem" width="100%" />
+                            ) : (
+                              templateDataNew && (
+                                <TemplateForm
+                                  templateDataNew={templateDataNew}
+                                  onInputChange={(value, variable) =>
+                                    setFormData((prev) => ({
+                                      ...prev,
+                                      [variable]: value,
+                                    }))
+                                  }
+                                  onImageUpload={handleImageUpload}
+                                  selectedOption={selectedOption}
+                                  fileHeaders={fileHeaders}
+                                  selectedTemplateData={selectedTemplateData}
+                                  onUrlIndexChange={setUrlIndex}
+                                  setVarLength={setVarLength}
+                                />
+                              )
+                            )}
+                          </div> */}
+    </div>
+  );
+};
