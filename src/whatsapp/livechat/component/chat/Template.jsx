8a870ccefa4da2f4116a2b60259333c0a93ff667
harish-sha <harish2002sharma@gmail.com>
2025-05-20 16:11:10 +0530

    set live chat ui and create whatsapp template fallback and response changes

--- src/whatsapp/livechat/component/chat/Template.jsx
+++ src/whatsapp/livechat/component/chat/Template.jsx
@@ -5,12 +5,18 @@ import {
 } from "@/apis/whatsapp/whatsapp";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { FaReply } from "react-icons/fa6";
+import { BsTelephoneFill } from "react-icons/bs";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 export const TemplateMessagePreview = ({ template }) => {
   const isImage = template?.templateType === "image";
   const isVideo = template?.templateType === "video";
   const isDocument = template?.templateType === "document";
   const isText = template?.templateType === "text";
+
+  const [isFetching, setIsFetching] = useState(false);
+
 
   const mediaPath = template?.mediaPath;
 
@@ -21,14 +27,15 @@ export const TemplateMessagePreview = ({ template }) => {
     }
   }, [template]);
 
-  useEffect(() => {
-    console.log(tempDetails);
-  }, [tempDetails]);
+  // useEffect(() => {
+  //   console.log(tempDetails);
+  // }, [tempDetails]);
 
   async function handleFetchSpecificTemplate() {
     const { wabaNumber, templateSrno, templateName } = template;
 
     try {
+      setIsFetching(true);
       const wabaList = await getWabaList();
       const matchedWaba = wabaList?.find((w) => w.mobileNo === wabaNumber);
 
@@ -48,12 +55,66 @@ export const TemplateMessagePreview = ({ template }) => {
     } catch (error) {
       console.error("Error fetching specific template:", error);
       toast.error("Something went wrong.");
+    } finally {
+      setIsFetching(false);
     }
   }
 
-  function renderMediaTemplate() {}
+  const getBtnIcon = (type) => {
+    switch (type) {
+      case "PHONE_NUMBER":
+        return <BsTelephoneFill className="mr-2" />;
+      case "QUICK_REPLY":
+        return <FaReply className="mr-2" />;
+      default:
+        return <FaExternalLinkAlt className="mr-2" />;
+    }
+  };
 
-  return (
+  const getBtnCss = (type) => {
+    switch (type) {
+      case "PHONE_NUMBER":
+        return "bg-blue-500 text-white";
+      case "QUICK_REPLY":
+        return "text-gray-800 bg-gray-200";
+      default:
+        return "bg-green-500 text-white";
+    }
+  };
+
+  const getBtnTitle = (type, phone, url, text) => {
+    switch (type) {
+      case "PHONE_NUMBER":
+        return `Contact us: ${phone}`;
+      case "QUICK_REPLY":
+        return `View more: ${text}`;
+      default:
+        return `Visit us: ${url}`;
+    }
+  };
+
+  function renderMediaTemplate() { }
+
+  const ButtonsGroup = ({ buttons }) => {
+    return (
+      <div className="flex flex-col gap-2 w-full max-w-[500px] mt-2">
+        {buttons.map(({ url, type, text, phone_number }, btnIndex) => (
+          <button
+            key={btnIndex}
+            title={url || phone_number}
+            className={`flex items-center justify-center px-4 py-2 text-sm cursor-pointer rounded-md w-full sm:w-auto ${getBtnCss(
+              type
+            )}`}
+          >
+            {getBtnIcon(type)}
+            <p className="ml-2">{text}</p>
+          </button>
+        ))}
+      </div>
+    );
+  };
+
+  return isFetching ? (
     // <div>
     //   <p className="text-sm">(TemplateMessage)</p>
     //   {isImage && (
@@ -79,16 +140,22 @@ export const TemplateMessagePreview = ({ template }) => {
     //     ></iframe>
     //   )}
     // </div>
-    <>
+    <div className="border border-gray-200 rounded-md w-90 p-5 bg-[#E1F3FB]">
       <p className="text-sm">(TemplateMessage)</p>
+      <h1>Loading...</h1>
+    </div>
+  ) : (
+    <>
       {tempDetails && (
-        <div className="border border-gray-200 rounded-md w-90 p-5 ">
+        <div className="border border-gray-200 rounded-md w-90 p-3 bg-[#E1F3FB]">
           {tempDetails?.map((item, index) => {
             if (item?.type === "HEADER" && item?.format === "IMAGE") {
               return (
                 <img
                   src={mediaPath}
                   alt={mediaPath}
+                  loading="lazy"
+                  key={index}
                   className={`h-50 w-auto select-none pointer-events-none border border-gray-200 rounded-md mr-auto ml-auto`}
                 />
               );
@@ -99,6 +166,7 @@ export const TemplateMessagePreview = ({ template }) => {
                   src={mediaPath}
                   controls={true}
                   autoPlay={false}
+                  key={index}
                   className={`h-45 m-auto border border-gray-200 rounded-md bg-center bg-no-repeat`}
                 />
               );
@@ -107,13 +175,17 @@ export const TemplateMessagePreview = ({ template }) => {
               return (
                 <iframe
                   src={mediaPath}
+                  key={index}
                   allow=" encrypted-media"
                   className={`h-48 border border-gray-200 rounded-md bg-center bg-no-repeat`}
                 ></iframe>
               );
             }
             if (item?.type === "BODY") {
-              return <p key={index}>{item?.text}</p>;
+              return <pre className="text-sm text-wrap " key={index}>{item?.text}</pre>;
+            }
+            if (item.type === "BUTTONS" && item?.buttons?.length > 0) {
+              return <ButtonsGroup buttons={item?.buttons} key={index} />
             }
           })}
         </div>
