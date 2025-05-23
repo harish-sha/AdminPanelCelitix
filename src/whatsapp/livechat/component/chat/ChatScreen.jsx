8daf9ec4a0fa15da05b9e312e251c55543141772
Arihant Jain <arihantj916@gmail.com>
2025-05-23 12:51:38 +0530

    feat: add max width to document fileName div

--- src/whatsapp/livechat/component/chat/ChatScreen.jsx
+++ src/whatsapp/livechat/component/chat/ChatScreen.jsx
@@ -355,8 +355,10 @@ export const ChatScreen = ({
                                       {getFileType(fileType)}
                                     </div>
                                     <div className="flex flex-col">
-                                      <div className="font-medium truncate">
-                                        {msg.fileName || "Untitled Document"}
+                                      <div className="font-medium truncate max-w-[10rem]">
+                                         {msg.fileName || "Untitled Document"}
+                                       {/* <p className="text-sm max-w-5">
+                                       </p> */}
                                       </div>
                                       <div className="text-xs text-gray-500 uppercase">
                                         .{fileType}
@@ -544,7 +546,7 @@ export const ChatScreen = ({
                   {getFileType(chatState.replyData.fileType)}
                 </div>
                 <div className="flex flex-col">
-                  <div className="font-medium truncate">
+                  <div className="font-medium truncate max-w-[10rem]">
                     {chatState.replyData.fileName || "Untitled Document"}
                   </div>
                   <div className="text-xs text-gray-500 uppercase">
@@ -604,7 +606,7 @@ export const ChatScreen = ({
                     {getFileType(selectedImage.fileType)}
                   </div>
                   <div className="flex flex-col">
-                    <div className="font-medium truncate break-words">
+                    <div className="font-medium truncate break-words max-w-[10rem]">
                       {selectedImage.fileName || "Untitled Document"}
                     </div>
                   </div>
