c21a760c4be4a00eb60afb2fdbeb7348c5c3fdd1
Arihant Jain <arihantj916@gmail.com>
2025-05-28 15:39:08 +0530

    feat: send message with new line

--- src/whatsapp/livechat/component/chat/Input.jsx
+++ src/whatsapp/livechat/component/chat/Input.jsx
@@ -72,14 +72,19 @@ export const ChatInput = ({
         <CustomEmojiPicker position="top" onSelect={insertEmoji} />
       </div>
       <div className="relative flex items-center justify-center w-full gap-2 border rounded-3xl">
-        <input
+        <textarea
           type="text"
-          className="flex-1 w-full p-2 focus:outline-none"
+          className="max-h-50 w-full p-2 focus:outline-none resize-none"
           placeholder="Type a message..."
           ref={inputRef}
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && e.shiftKey) {
+              e.preventDefault();
+              sendMessage();
+            }
+          }}
         />
         <button
           onClick={sendMessage}
@@ -126,8 +131,9 @@ export const ChatInput = ({
       <div className="relative ml-4">
         <button
           onClick={() => setIsSpeedDialOpen(!isSpeedDialOpen)}
-          className={`flex items-center justify-center w-8 h-8 cursor-pointer bg-[#22577E] text-white rounded-full shadow-md transition-transform ${isSpeedDialOpen ? "rotate-45" : ""
-            }`}
+          className={`flex items-center justify-center w-8 h-8 cursor-pointer bg-[#22577E] text-white rounded-full shadow-md transition-transform ${
+            isSpeedDialOpen ? "rotate-45" : ""
+          }`}
         >
           <FaPlus />
         </button>
