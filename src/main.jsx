80a921f604cfb9fff1b1e672dcd284211c592248
Arihant Jain <arihantj916@gmail.com>
2025-06-05 14:35:50 +0530

    feat: wrap chidren in usePageData provider

--- src/main.jsx
+++ src/main.jsx
@@ -10,22 +10,26 @@ import { UserProvider } from "./context/auth";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DownloadProvider } from "./context/DownloadProvider.jsx";
+import { PageDataProvider } from "./context/page.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* User Context */}
     <UserProvider>
-      {/* Prime React */}
-      <PrimeReactProvider>
-        {/* Error Boundary*/}
-        <ErrorBoundary>
-          <DownloadProvider>
-            <DndProvider backend={HTML5Backend}>
-              <App />
-            </DndProvider>
-          </DownloadProvider>
-        </ErrorBoundary>
-      </PrimeReactProvider>
+      <PageDataProvider>
+        {/* Prime React */}
+
+        <PrimeReactProvider>
+          {/* Error Boundary*/}
+          <ErrorBoundary>
+            <DownloadProvider>
+              <DndProvider backend={HTML5Backend}>
+                <App />
+              </DndProvider>
+            </DownloadProvider>
+          </ErrorBoundary>
+        </PrimeReactProvider>
+      </PageDataProvider>
     </UserProvider>
   </StrictMode>
 );
