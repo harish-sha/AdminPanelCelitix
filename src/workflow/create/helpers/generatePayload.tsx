107824a1fab80ea749f31b9f40eb23b05a6116e9
Arihant Jain <arihantj916@gmail.com>
2025-06-23 17:19:05 +0530

    feat: update generate payload add fallback

--- src/workflow/create/helpers/generatePayload.tsx
+++ src/workflow/create/helpers/generatePayload.tsx
@@ -1,3 +1,7 @@
+function capitalizeFirstLetter(val) {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
+
 export const generatePayload = (
   nodesInputData: Record<string, any>,
   nodes: any[],
@@ -5,20 +9,20 @@ export const generatePayload = (
 ) => {
   const payload = [];
 
-  nodes.forEach((node) => {
+  nodes.forEach((node, index) => {
     const originalType = node.type;
-    const value = `${originalType}_${node.id}`;
+    const value = `${originalType}_${index}`;
     const nodeInput = nodesInputData[node.id] || {};
 
-    // //prevNode
-    // const incomingEdge = edges.find((edge) => edge.target === node.id);
-    // const prevNode = incomingEdge
-    //   ? `${
-    //       nodes.find((n) => n.id === incomingEdge.source)?.type === "starting"
-    //         ? "START"
-    //         : nodes.find((n) => n.id === incomingEdge.source)?.type
-    //     }_${incomingEdge.source}`
-    //   : "";
+    //prevNode
+    const incomingEdge = edges.find((edge) => edge.target === node.id);
+    const prevNode = incomingEdge
+      ? `${
+          nodes.find((n) => n.id === incomingEdge.source)?.type === "starting"
+            ? "START"
+            : nodes.find((n) => n.id === incomingEdge.source)?.type
+        }_${incomingEdge.source}`
+      : "";
 
     //nextNodes
     const outgoingEdges = edges.filter((edge) => edge.source === node.id);
@@ -31,39 +35,51 @@ export const generatePayload = (
       })
       .filter(Boolean);
 
-    // console.log("nextNodes", nextNodes);
-    // console.log("outgoingEdges", outgoingEdges);
-
     const entry: Record<string, any> = {
       nodeType: originalType,
       nodeId: value,
-      nodeIndex: node.id,
+      nodeIndex: String(node.id),
       position_top: `${node.position.y}px`,
       position_left: `${node.position.x}px`,
-      isLastNode: 1,
+      isLastNode: nextNodes.length === 0 ? 1 : 0,
       workflowType: -1,
       [value]: {
-        conditionList: {},
+        // conditionList: {},
       },
     };
 
     nodeInput?.options?.forEach((input: any, index: number) => {
       const id = `${value}_condition_${index}`;
-      entry[value].conditionList[id] = {
-        value: {
-          type: input?.type,
-          val: input?.value,
-          time: input?.time,
-          otpTimeIntervalValue: 0,
-          keyPressValue: "",
-          callDurationTime: input?.time || "",
-          ansPreFix: input?.ansPreFix || "",
+      entry[value].conditionList = {
+        [id]: {
+          value: {
+            type: input?.type,
+            val: input?.value,
+            time: input?.time || 0,
+            otpTimeIntervalValue: 0,
+            keyPressValue: "-1",
+            callDurationTime: input?.callDurationTime || 0,
+            ansPreFix: input?.ansPreFix || -1,
+          },
+          ...(nextNodes[index] && { nextNode: nextNodes[index] }),
         },
-        nextNode: nextNodes[index] || "",
       };
     });
 
-    // console.log("entry", entry);
+    if (originalType === "whatsapp") {
+      entry[value].value = {
+        wabanumber: String(nodeInput?.wabaSrno),
+        whatsappTemplate: String(nodeInput?.whatsappTemplate),
+        whatsapp_category: nodeInput?.whatsapp_category,
+        whatsapp_templateType: nodeInput?.whatsapp_templateType,
+        variables: nodeInput?.variables,
+        fileInput: nodeInput?.fileInput,
+        urlValues: nodeInput?.urlValues,
+      };
+    }
+
+    // entry[prevNode] = prevNode;
+
     payload.push(entry);
     // console.log("nodeInput", nodeInput);
   });
