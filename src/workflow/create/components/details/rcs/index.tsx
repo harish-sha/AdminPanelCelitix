eac0174226db5281b85e4550aff7c7c491899e7d
Arihant Jain <arihantj916@gmail.com>
2025-06-23 17:40:36 +0530

    feat: add component to manage RCS basic Details

--- src/workflow/create/components/details/rcs/index.tsx
+++ src/workflow/create/components/details/rcs/index.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+import {
+  fetchAllAgents,
+  fetchAllTemplates,
+  fetchTemplateDetails,
+} from "@/apis/rcs/rcs";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { HandleCampaignDetails } from "./handleCampaignDetails";
 
 export const RCS = ({
   id,
@@ -11,5 +18,131 @@ export const RCS = ({
   setNodesInputData: React.Dispatch<React.SetStateAction<{}>>;
   setDetailsDialogVisible: React.Dispatch<React.SetStateAction<{}>>;
 }) => {
-  return <>RCS Node</>;
+  //basic Details
+  const [allAgents, setAllAgents] = useState([]);
+  const [allTemplates, setAllTemplates] = useState([]);
+  const [selectedTemplate, setSelectedTemplate] = useState("");
+  const [campaignDetails, setCampaignDetails] = useState({
+    agent: "",
+    campaignName: "",
+  });
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  //variable
+  const [varLength, setVarLength] = useState(0);
+  const [varList, setVarList] = useState([]);
+  const [inputVariables, setInputVariables] = useState([]);
+
+  //btnVar
+  const [btnvarLength, setBtnVarLength] = useState(0);
+  const [btnvarList, setBtnVarList] = useState([]);
+  const [btninputVariables, setBtnInputVariables] = useState([]);
+
+  //preview
+  const [templateDetails, setTemplateDetails] = useState([]);
+
+  const [carVar, setCarVar] = useState({
+    length: 0,
+    data: {},
+  });
+  const [carVarInput, setCarVarInput] = useState([]);
+  const [headers, setHeaders] = useState([]);
+  const [finalVarList, setFinalVarList] = useState([]);
+
+  function extractVariable(data) {
+    if (!data || !Array.isArray(data)) return;
+
+    if (data.length === 1) {
+      const content = data[0]?.content || "";
+      const suggestionVar = data[0]?.suggestions?.map((item) => {
+        if (item?.type === "website") {
+          const match = item?.suggestionValue.match(/{#(.+?)#}/g) || [];
+          setBtnVarLength(match.length);
+          setBtnVarList(match);
+        }
+      });
+      const matches = content.match(/{#(.+?)#}/g) || [];
+
+      setVarLength(matches.length);
+      setVarList(matches);
+    }
+
+    if (data.length > 1) {
+      const result = data.reduce(
+        (acc, item, index) => {
+          const content = item?.content || "";
+          const matches = content.match(/{#(.+?)#}/g) || [];
+
+          acc.totalLength += matches.length;
+          acc.data[index] = matches;
+
+          return acc;
+        },
+        { totalLength: 0, data: {} }
+      );
+
+      setCarVar({ length: result.totalLength, data: result.data });
+    }
+  }
+
+  useEffect(() => {
+    async function handleFetchAllAgents() {
+      try {
+        const res = await fetchAllAgents();
+        setAllAgents(res);
+      } catch (e) {
+        toast.error("Something went wrong.");
+      }
+    }
+
+    handleFetchAllAgents();
+  }, []);
+
+  useEffect(() => {
+    async function handleFetchAllTemplates() {
+      try {
+        const res = await fetchAllTemplates(campaignDetails?.agent, "1");
+        setAllTemplates(res?.Data);
+      } catch (e) {
+        toast.error("Something went wrong.");
+      }
+    }
+
+    handleFetchAllTemplates();
+  }, [campaignDetails]);
+
+  useEffect(() => {
+    async function handleFetchTemplateDetails() {
+      if (!selectedTemplate) return;
+      try {
+        const res = await fetchTemplateDetails(selectedTemplate);
+        extractVariable(res);
+        setTemplateDetails(res);
+      } catch (e) {
+        toast.error("Something went wrong.");
+      }
+    }
+  }, [selectedTemplate]);
+
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-3 mt-5">
+        <div className=" p-3 bg-gray-100 rounded-lg shadow-md lg:flex-1">
+          <HandleCampaignDetails
+            setCampaignDetails={setCampaignDetails}
+            campaignDetails={campaignDetails}
+            allAgents={allAgents}
+            allTemplates={allTemplates}
+            setTemplateDetails={setTemplateDetails}
+            setVarList={setVarList}
+            setInputVariables={setInputVariables}
+            setVarLength={setVarLength}
+            setCarVar={setCarVar}
+            setSelectedTemplate={setSelectedTemplate}
+            selectedTemplate={selectedTemplate}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
