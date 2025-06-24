dca93ae7b7320945e78436d28817499999b6160e
Arihant Jain <arihantj916@gmail.com>
2025-06-24 12:09:46 +0530

    feat: add component to render TTS OBD template

--- src/workflow/create/components/details/obd/index.tsx
+++ src/workflow/create/components/details/obd/index.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+import InputField from "@/components/layout/InputField";
+import AnimatedDropdown from "@/whatsapp/components/AnimatedDropdown";
+import UniversalTextArea from "@/whatsapp/components/UniversalTextArea";
+import { RadioButton } from "primereact/radiobutton";
+import React, { useState } from "react";
+import { TTS } from "./tts";
 
 export const OBD = ({
   id,
@@ -11,5 +16,130 @@ export const OBD = ({
   setNodesInputData: React.Dispatch<React.SetStateAction<{}>>;
   setDetailsDialogVisible: React.Dispatch<React.SetStateAction<{}>>;
 }) => {
-  return <>OBD</>;
+  const [basicDetails, setBasicDetails] = useState({
+    type: "transactional",
+    name: "",
+    templateType: "",
+    tts: "",
+  });
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="flex flex-wrap bg-[#E5E7EB] rounded-2xl">
+          <div className=" w-full  p-3  rounded-xl flex lg:flex-nowrap flex-wrap gap-6  min-h-[80vh]">
+            <div className="lg:w-1/2 w-full rounded-xl bg-[#f9f9f9]">
+              <div className="flex items-center justify-around gap-4 px-2 py-4 ">
+                <div className="flex gap-2 border border-gray-300 rounded-lg w-1/2 px-3 py-2">
+                  <RadioButton
+                    inputId="radioOptionTransactional"
+                    name="radioGroupCampaign"
+                    value="transactional"
+                    onChange={() => {
+                      setBasicDetails((prev) => ({
+                        ...prev,
+                        type: "transactional",
+                      }));
+                    }}
+                    checked={basicDetails.type === "transactional"}
+                  />
+                  <label
+                    htmlFor="radioOptionTransactional"
+                    className="text-gray-700 font-medium text-sm   cursor-pointer"
+                  >
+                    Transactional
+                  </label>
+                </div>
+                <div className="flex gap-2 border border-gray-300 rounded-lg w-1/2 px-3 py-2">
+                  <RadioButton
+                    inputId="radioOptionPromotional"
+                    name="radioGroupCampaign"
+                    value="promotional"
+                    onChange={() => {
+                      setBasicDetails((prev) => ({
+                        ...prev,
+                        type: "promotional",
+                      }));
+                    }}
+                    checked={basicDetails.type === "promotional"}
+                  />
+                  <label
+                    htmlFor="radioOptionPromotional"
+                    className="text-gray-700 font-medium text-sm cursor-pointer"
+                  >
+                    Promotional
+                  </label>
+                </div>
+              </div>
+
+              <div className="p-3">
+                <div>
+                  <InputField
+                    label="Campaign Name"
+                    id="campaignName"
+                    name="campaignName"
+                    value={basicDetails.name}
+                    onChange={(e) =>
+                      setBasicDetails((prev) => ({
+                        ...prev,
+                        name: e.target.value,
+                      }))
+                    }
+                    placeholder="Enter Campaign name..."
+                    tooltipContent="Enter a name for your campaign to easily identify it later"
+                    maxLength={20}
+                  />
+                </div>
+
+                <div className="my-4">
+                  <AnimatedDropdown
+                    label="OBD Type:"
+                    id="obdType"
+                    name="obdType"
+                    options={[
+                      { value: "texttospeech", label: "Text To Speech" },
+                      {
+                        value: "simplebroadcast",
+                        label: "Simple Broadcast",
+                      },
+                      { value: "multibroadcast", label: "Multi Broadcast" },
+                      {
+                        value: "dynamicbroadcast",
+                        label: "Dynamic Broadcast",
+                      },
+                    ]}
+                    value={basicDetails.templateType}
+                    onChange={(value) => {
+                      setBasicDetails((prev) => ({
+                        ...prev,
+                        templateType: value,
+                      }));
+                      // setTTSArea("");
+                      // setSelectedSBVoiceFile(null);
+
+                      // setSelectedMBFiletwo(null);
+                      // setVoiceDBClip(null);
+                      // setRetry(null);
+                      setInterval(null);
+                    }}
+                    placeholder="Select Template"
+                    tooltipContent="Use a pre-defined template for your voice broadcast.
+                                      Text To Speech Convert text messages to speech for the broadcast.
+                                      Simple Broadcast Broadcast a single message to all recipients.
+                                      Multi Broadcast Broadcast multiple messages in a sequence."
+                    disabled={false}
+                  />
+                </div>
+                {basicDetails?.templateType === "texttospeech" && (
+                  <TTS
+                    setBasicDetails={setBasicDetails}
+                    basicDetails={basicDetails}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
