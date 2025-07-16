8394edfc057221a14e653dcb2ff3ee2867279f0b
Arihant Jain <arihantj916@gmail.com>
2025-07-16 11:07:53 +0530

    feat: display all operators and apply filters

--- src/admin/managePrefix/pages/AddOperator.jsx
+++ src/admin/managePrefix/pages/AddOperator.jsx
@@ -1,19 +1,21 @@
-import { Box, Button, IconButton, Paper, Typography } from '@mui/material';
-import { DataGrid, GridFooterContainer } from '@mui/x-data-grid';
-import React, { useState } from 'react'
-import CustomNoRowsOverlay from '../../../whatsapp/components/CustomNoRowsOverlay';
-import styled from 'styled-components';
-import usePagination from '@mui/material/usePagination/usePagination';
-import CustomTooltip from '../../../whatsapp/components/CustomTooltip';
+import { Box, Button, IconButton, Paper, Typography } from "@mui/material";
+import { DataGrid, GridFooterContainer } from "@mui/x-data-grid";
+import React, { useEffect, useState } from "react";
+import CustomNoRowsOverlay from "../../../whatsapp/components/CustomNoRowsOverlay";
+import styled from "styled-components";
+import usePagination from "@mui/material/usePagination/usePagination";
+import CustomTooltip from "../../../whatsapp/components/CustomTooltip";
 import { MdOutlineDeleteForever } from "react-icons/md";
 import EditNoteIcon from "@mui/icons-material/EditNote";
-import DropdownWithSearch from '../../../whatsapp/components/DropdownWithSearch';
-import UniversalButton from '../../../whatsapp/components/UniversalButton';
-import { Dialog } from 'primereact/dialog';
-import InputField from '../../../whatsapp/components/InputField';
+import DropdownWithSearch from "../../../whatsapp/components/DropdownWithSearch";
+import UniversalButton from "../../../whatsapp/components/UniversalButton";
+import { Dialog } from "primereact/dialog";
+import InputField from "../../../whatsapp/components/InputField";
 import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
 import { toast } from "react-hot-toast";
 import * as XLSX from "xlsx";
+import { getOperatorList } from "@/apis/admin/admin";
+import { getCountryList } from "@/apis/common/common";
 
 const PaginationList = styled("ul")({
   listStyle: "none",
@@ -89,6 +91,10 @@ const AddOperator = ({ id, name }) => {
     pageSize: 10,
   });
 
+  const [operatorList, setOperatorList] = useState([]);
+  const [countryList, setCountryList] = useState([]);
+
+  const [searchCountry, setSearchCountry] = useState("");
   const handleAdd = () => {
     setNewOperator(true);
   };
@@ -102,29 +108,29 @@ const AddOperator = ({ id, name }) => {
   };
 
   const countryOptions = [
-    { label: 'India', value: 'India' },
-    { label: 'USA', value: 'USA' },
-    { label: 'UK', value: 'UK' },
-    { label: 'Australia', value: 'Australia' },
-  ]
+    { label: "India", value: "India" },
+    { label: "USA", value: "USA" },
+    { label: "UK", value: "UK" },
+    { label: "Australia", value: "Australia" },
+  ];
   const AddnewcountryOptions = [
-    { label: 'India', value: 'India' },
-    { label: 'USA', value: 'USA' },
-    { label: 'UK', value: 'UK' },
-    { label: 'Australia', value: 'Australia' },
-  ]
+    { label: "India", value: "India" },
+    { label: "USA", value: "USA" },
+    { label: "UK", value: "UK" },
+    { label: "Australia", value: "Australia" },
+  ];
   const AddeditcountryOptions = [
-    { label: 'India', value: 'India' },
-    { label: 'USA', value: 'USA' },
-    { label: 'UK', value: 'UK' },
-    { label: 'Australia', value: 'Australia' },
-  ]
+    { label: "India", value: "India" },
+    { label: "USA", value: "USA" },
+    { label: "UK", value: "UK" },
+    { label: "Australia", value: "Australia" },
+  ];
   const AddimportcountryOptions = [
-    { label: 'India', value: 'India' },
-    { label: 'USA', value: 'USA' },
-    { label: 'UK', value: 'UK' },
-    { label: 'Australia', value: 'Australia' },
-  ]
+    { label: "India", value: "India" },
+    { label: "USA", value: "USA" },
+    { label: "UK", value: "UK" },
+    { label: "Australia", value: "Australia" },
+  ];
 
   // handle File drop
   const handleFileDrop = (event) => {
@@ -237,45 +243,68 @@ const AddOperator = ({ id, name }) => {
     toast.success("File removed successfully.");
   };
 
+  async function handleFetchOpertorList() {
+    try {
+      const res = await getOperatorList();
+      const countryList = await getCountryList();
+      setCountryList(countryList);
 
+      const countryMap = new Map(
+        countryList.map((country) => [country.srNo, country.countryName])
+      );
 
-  const rows = Array.from({ length: 20 }, (_, i) => ({
-    id: i + 1,
-    sn: i + 1,
-    country: "India",
-    operator: "Ashima",
-  }));
+      const enrichedOperators = res.map((operator) => ({
+        ...operator,
+        countryName: countryMap.get(operator.countrySrno) || null,
+      }));
+
+      const filteredOperators = enrichedOperators.filter(
+        (operator) => operator.countrySrno === searchCountry || searchCountry === ""
+      );
+      setOperatorList(filteredOperators);
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  }
+
+  useEffect(() => {
+    handleFetchOpertorList();
+  }, []);
+
+  const rows = Array.isArray(operatorList)
+    ? operatorList.map((item, i) => ({
+        ...item,
+        id: item.srNo,
+        sn: i + 1,
+      }))
+    : [];
 
   const columns = [
     { field: "sn", headerName: "S.No", flex: 0, minWidth: 50 },
-    { field: "country", headerName: "Country", flex: 1, minWidth: 80 },
-    { field: "operator", headerName: "Operator", flex: 1, minWidth: 120 },
+    { field: "countryName", headerName: "Country", flex: 1, minWidth: 80 },
+    { field: "operatorName", headerName: "Operator", flex: 1, minWidth: 120 },
     {
-      field: 'action',
-      headerName: 'Action',
+      field: "action",
+      headerName: "Action",
       flex: 1,
       minWidth: 100,
       renderCell: (params) => (
         <>
-          <CustomTooltip
-            title="Edit Operator"
-            placement="top"
-            arrow
-          >
+          <CustomTooltip title="Edit Operator" placement="top" arrow>
             <IconButton onClick={() => handleEdit(params.row)}>
               <EditNoteIcon
                 sx={{
-                  fontSize: '1.2rem',
-                  color: 'gray',
-                }} />
+                  fontSize: "1.2rem",
+                  color: "gray",
+                }}
+              />
             </IconButton>
           </CustomTooltip>
-          <CustomTooltip
-            title="Delete Operator"
-            placement="top"
-            arrow
-          >
-            <IconButton className='no-xs' onClick={() => handleDelete(params.row)}>
+          <CustomTooltip title="Delete Operator" placement="top" arrow>
+            <IconButton
+              className="no-xs"
+              onClick={() => handleDelete(params.row)}
+            >
               <MdOutlineDeleteForever
                 className="text-red-500 cursor-pointer hover:text-red-600"
                 size={20}
@@ -290,7 +319,6 @@ const AddOperator = ({ id, name }) => {
   const totalPages = Math.ceil(rows.length / paginationModel.pageSize);
 
   const CustomFooter = () => {
-
     return (
       <GridFooterContainer
         sx={{
@@ -304,7 +332,6 @@ const AddOperator = ({ id, name }) => {
         }}
       >
         <Box
-
           sx={{
             display: "flex",
             alignItems: "center",
@@ -345,14 +372,19 @@ const AddOperator = ({ id, name }) => {
   return (
     <div>
       <div className="flex flex-wrap gap-2 items-end justify-between pb-3 w-full">
-        <div className='flex flex-wrap gap-2 items-end'>
+        <div className="flex flex-wrap gap-2 items-end">
           <div className="w-56">
             <DropdownWithSearch
               label="Country"
               id="country"
               name="country"
-              options={countryOptions}
-              placeholder='select country'
+              options={countryList.map((country) => ({
+                label: country.countryName,
+                value: country.srNo,
+              }))}
+              placeholder="select country"
+              value={searchCountry}
+              onChange={(e) => setSearchCountry(e)}
             />
           </div>
           <div className="w-max-content">
@@ -360,11 +392,11 @@ const AddOperator = ({ id, name }) => {
               label="Search"
               id="searchaddoperator"
               name="searchaddoperator"
-            // onClick={handleSearchAddOperator}
+              onClick={handleFetchOpertorList}
             />
           </div>
         </div>
-        <div className='flex gap-2'>
+        <div className="flex gap-2">
           <div className="w-max-content">
             <UniversalButton
               label="Add New"
@@ -372,7 +404,6 @@ const AddOperator = ({ id, name }) => {
               name="addnewoperator"
               onClick={handleAdd}
             />
-
           </div>
           <div className="w-max-content">
             <UniversalButton
@@ -381,7 +412,6 @@ const AddOperator = ({ id, name }) => {
               name="importoperator"
               onClick={handleImport}
             />
-
           </div>
         </div>
       </div>
@@ -402,7 +432,7 @@ const AddOperator = ({ id, name }) => {
             noRowsOverlay: CustomNoRowsOverlay,
           }}
           onRowSelectionModelChange={(ids) => setSelectedRows(ids)}
-          checkboxSelection
+          // checkboxSelection
           disableRowSelectionOnClick
           disableColumnResize
           disableColumnMenu
@@ -429,21 +459,21 @@ const AddOperator = ({ id, name }) => {
         className="lg:w-[30rem] md:w-[25rem] w-[20rem]"
         draggable={false}
       >
-        <div className='space-y-4'>
+        <div className="space-y-4">
           <DropdownWithSearch
             label="Country"
             id="countryaddnew"
             name="countryaddnew"
             options={AddnewcountryOptions}
-            placeholder='select country'
+            placeholder="select country"
           />
           <InputField
             label="Operator Name*"
             id="addnewoperatorname"
             name="addnewoperatorname"
-            placeholder='Enter Operator Name'
+            placeholder="Enter Operator Name"
           />
-          <div className='flex justify-center'>
+          <div className="flex justify-center">
             <UniversalButton
               label="Save"
               id="saveaddprefix"
@@ -459,21 +489,21 @@ const AddOperator = ({ id, name }) => {
         className="lg:w-[30rem] md:w-[25rem] w-[20rem]"
         draggable={false}
       >
-        <div className='space-y-4'>
+        <div className="space-y-4">
           <DropdownWithSearch
             label="Country"
             id="countryaddedit"
             name="countryaddedit"
             options={AddeditcountryOptions}
-            placeholder='select country'
+            placeholder="select country"
           />
           <InputField
             label="Operator Name*"
             id="addeditoperatorname"
             name="addeditoperatorname"
-            placeholder='Enter Operator Name'
+            placeholder="Enter Operator Name"
           />
-          <div className='flex justify-center'>
+          <div className="flex justify-center">
             <UniversalButton
               label="Save"
               id="saveeditprefix"
@@ -489,13 +519,13 @@ const AddOperator = ({ id, name }) => {
         className="lg:w-[30rem] md:w-[25rem] w-[20rem]"
         draggable={false}
       >
-        <div className='space-y-4'>
+        <div className="space-y-4">
           <DropdownWithSearch
             label="Country"
             id="countryimport"
             name="countryimport"
             options={AddimportcountryOptions}
-            placeholder='select country'
+            placeholder="select country"
           />
           <div className="file-upload mt-2">
             <div
@@ -522,8 +552,9 @@ const AddOperator = ({ id, name }) => {
                   <button
                     onClick={handleFileUpload}
                     disabled={isUploading}
-                    className={`px-2 py-1.5 bg-green-400 rounded-lg hover:bg-green-500 cursor-pointer ${isUploading ? "disabled" : ""
-                      }`}
+                    className={`px-2 py-1.5 bg-green-400 rounded-lg hover:bg-green-500 cursor-pointer ${
+                      isUploading ? "disabled" : ""
+                    }`}
                   >
                     <FileUploadOutlinedIcon
                       sx={{ color: "white", fontSize: "23px" }}
@@ -560,7 +591,7 @@ const AddOperator = ({ id, name }) => {
               </div>
             </div>
           </div>
-          <div className='flex justify-center'>
+          <div className="flex justify-center">
             <UniversalButton
               label="Submit"
               id="submitimportprefix"
@@ -570,7 +601,7 @@ const AddOperator = ({ id, name }) => {
         </div>
       </Dialog>
     </div>
-  )
-}
+  );
+};
 
-export default AddOperator
+export default AddOperator;
