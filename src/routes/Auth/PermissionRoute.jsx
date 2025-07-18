fbc4fb97aa5457d17c1f83cbf9f32c114333292b
Arihant Jain <arihantj916@gmail.com>
2025-07-18 11:18:34 +0530

    feat: optimize permissionRoutes file

--- src/routes/Auth/PermissionRoute.jsx
+++ src/routes/Auth/PermissionRoute.jsx
@@ -1,244 +1,481 @@
+// import { useUser } from "@/context/auth";
+// import { useLocation } from "react-router-dom";
+// import { Navigate, Outlet } from "react-router-dom";
+// import PageNotFound from "@/NotFound/PageNotFound";
+
+// export const PermissionRoute = ({ children }) => {
+//   const { user } = useUser();
+//   const location = useLocation();
+//   const menuItems = [
+//     { name: "Home", links: "/", roles: ["ADMIN"] },
+//     {
+//       name: "SMS",
+//       links: [
+//         "/sendsms",
+//         "/smsreports",
+//         "/smsdlttemplates",
+//         "/smscampaigndetaillogs",
+//         "/smsAttachmentdetaillog",
+//       ],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "Two Way SMS",
+//       links: ["/managekeywords", "/twowayreports", "/twowayintegration"],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "RCS",
+//       links: [
+//         "/sendrcs",
+//         "/rcsmanagetemplate",
+//         "/rcssuggestionreport",
+//         "/rcsdeliverycampaigndetails",
+//         "/rcsaddtemplatercs",
+//         "/rcsdeliveryreport",
+//         "/rcsmanagebot",
+//         "/rcslivechats",
+//       ],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "WHATSAPP",
+//       links: [
+//         "/wlaunchcampaign",
+//         "/wlivechat",
+//         "/wmanagecampaign",
+//         "/managetemplate",
+//         "/wqrcode",
+//         "/wmanagewaba",
+//         "/wwhatsappconversation",
+//         "/wwhatsappmanageagent",
+//         "/wwhatsappbot",
+//         "/createwhatsappbot",
+//         "/wcampaigndetailsreport",
+//         "/createtemplate",
+//         "/wlcsetting",
+//         "/wwhatsappflows",
+//         "/wflowcreation",
+//         "/apicampaigninfo",
+//         "/wblockuser",
+//       ],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "Number Lookup",
+//       links: ["/hlrlookup", "/lookupreports"],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "App Authenticator",
+//       links: ["/authsettings", "/authreports"],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "E-mail",
+//       links: ["/emailtemplate", "/emailreports"],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "OBD",
+//       links: [
+//         "/obdcreatecampaign",
+//         "/obdmanagecampaign",
+//         "/obdmanagevoiceclips",
+//         "/obdIntegration",
+//       ],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "IBD",
+//       links: [
+//         "/ibdcallhistory",
+//         "/ibdmanageexecutive",
+//         "/ibdivrflow",
+//         "/ibdsettings",
+//       ],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "Missed Call",
+//       links: ["/missedcallhistory", "/missedcallsettings"],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "Click-2-Call",
+//       links: ["/clicktohistory", "/clicktosettings"],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "CallBack",
+//       links: ["/callback", "/addcallback", "/editcallback"],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "Manage Funds",
+//       links: ["/recharge"],
+//       roles: ["ADMIN", "DIRECTUSER"],
+//     },
+//     {
+//       name: "Admin",
+//       links: [
+//         "/manageuser",
+//         "/managedlttemplate",
+//         "/rcsmanagebot",
+//         "/managevoiceclips",
+//         "/manageplan",
+//         "/accountmanager",
+//         "/graphmain",
+//         "/graphuserwise",
+//         "/manageSMPP",
+//         "/managerouting",
+//         "/SMPPerrorcode",
+//         "/manageprefix",
+//         "/blacklist",
+//         "/managenotifications",
+//         "/CreateWhatsappTemplateAdmin",
+//         "/manageadduser",
+//       ],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "Manage Contacts",
+//       links: "/managecontacts",
+//       roles: ["ADMIN", "DIRECTUSER"],
+//     },
+//     {
+//       name: "Wish Management",
+//       links: "/smswishmanagement",
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "API Docs",
+//       links: "/docs",
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "openRoutes",
+//       links: [
+//         "/download",
+//         "/loginIpdetails",
+//         "/profile",
+//         "/settings",
+//         "/transactions",
+//         "/tagmanager",
+//       ],
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "Download",
+//       links: "/dummy",
+//       roles: ["ADMIN"],
+//     },
+//     {
+//       name: "WorkFlow",
+//       links: ["/workflow", "/workflow/create", "/workflow/edit"],
+
+//       roles: ["ADMIN"],
+//     },
+//   ];
+
+//   const currentPath = location.pathname;
+
+//   const getPrivateRoute = (menuItems, userState) => {
+//     let allowedServices = [];
+
+//     if (userState.role === "ADMIN") {
+//       return menuItems;
+//     }
+//     if (userState.role === "AGENT") {
+//       return [
+//         {
+//           name: "Home",
+//           links: "/",
+//           roles: ["AGENT"],
+//         },
+//         {
+//           name: "WhatsApp LiveChat",
+//           links: "/wlivechat",
+//           roles: ["AGENT"],
+//         },
+//         {
+//           name: "openRoutes",
+//           links: ["/profile", "/settings"],
+//           roles: ["ADMIN"],
+//         },
+//       ];
+//     }
+
+//     menuItems.forEach((item) => {
+//       // if (item.roles.includes(userState.role)) allowedServices.push(item);
+//       userState.services.forEach((service, index) => {
+//         if (item.name == service.display_name) {
+//           allowedServices.push(item);
+//         }
+//       });
+//       if (item.name === "Home") {
+//         allowedServices.push(item);
+//       }
+//       if (item.name === "apiDocs") {
+//         allowedServices.push(item);
+//       }
+//       if (item.name === "CallBack") {
+//         allowedServices.push(item);
+//       }
+//       if (item.name === "openRoutes") {
+//         allowedServices.push(item);
+//       }
+//       if (item.name === "Manage Contacts") {
+//         allowedServices.push(item);
+//       }
+//       if (item.name === "WorkFlow") {
+//         allowedServices.push(item);
+//       }
+//     });
+
+//     return allowedServices;
+//   };
+
+//   const privateRoute = getPrivateRoute(menuItems, user);
+
+//   if (!privateRoute.some((item) => item?.links?.includes(currentPath)))
+//     return <PageNotFound />;
+//   return <Outlet />;
+// };
+
 import { useUser } from "@/context/auth";
-import { useLocation } from "react-router-dom";
-import { Navigate, Outlet } from "react-router-dom";
+import { useLocation, Outlet } from "react-router-dom";
 import PageNotFound from "@/NotFound/PageNotFound";
+import { useMemo } from "react";
+import Loader from "@/whatsapp/components/Loader";
 
-export const PermissionRoute = ({ children }) => {
-  const { user } = useUser();
+export const PermissionRoute = () => {
+  const { user, isLoading } = useUser();
   const location = useLocation();
-  const menuItems = [
-    { name: "Home", links: "/", roles: ["ADMIN"] },
-    {
-      name: "SMS",
-      links: [
-        "/sendsms",
-        "/smsreports",
-        "/smsdlttemplates",
-        "/smscampaigndetaillogs",
-        "/smsAttachmentdetaillog",
-      ],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "Two Way SMS",
-      links: ["/managekeywords", "/twowayreports", "/twowayintegration"],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "RCS",
-      links: [
-        "/sendrcs",
-        "/rcsmanagetemplate",
-        "/rcssuggestionreport",
-        "/rcsdeliverycampaigndetails",
-        "/rcsaddtemplatercs",
-        "/rcsdeliveryreport",
-        "/rcsmanagebot",
-        "/rcslivechats",
-      ],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "WHATSAPP",
-      links: [
-        "/wlaunchcampaign",
-        "/wlivechat",
-        "/wmanagecampaign",
-        "/managetemplate",
-        "/wqrcode",
-        "/wmanagewaba",
-        "/wwhatsappconversation",
-        "/wwhatsappmanageagent",
-        "/wwhatsappbot",
-        "/createwhatsappbot",
-        "/wcampaigndetailsreport",
-        "/createtemplate",
-        "/wlcsetting",
-        "/wwhatsappflows",
-        "/wflowcreation",
-        "/apicampaigninfo",
-        "/wblockuser",
-      ],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "Number Lookup",
-      links: ["/hlrlookup", "/lookupreports"],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "App Authenticator",
-      links: ["/authsettings", "/authreports"],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "E-mail",
-      links: ["/emailtemplate", "/emailreports"],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "OBD",
-      links: [
-        "/obdcreatecampaign",
-        "/obdmanagecampaign",
-        "/obdmanagevoiceclips",
-        "/obdIntegration",
-      ],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "IBD",
-      links: [
-        "/ibdcallhistory",
-        "/ibdmanageexecutive",
-        "/ibdivrflow",
-        "/ibdsettings",
-      ],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "Missed Call",
-      links: ["/missedcallhistory", "/missedcallsettings"],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "Click-2-Call",
-      links: ["/clicktohistory", "/clicktosettings"],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "CallBack",
-      links: ["/callback", "/addcallback", "/editcallback"],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "Manage Funds",
-      links: ["/recharge"],
-      roles: ["ADMIN", "DIRECTUSER"],
-    },
-    {
-      name: "Admin",
-      links: [
-        "/manageuser",
-        "/managedlttemplate",
-        "/rcsmanagebot",
-        "/managevoiceclips",
-        "/manageplan",
-        "/accountmanager",
-        "/graphmain",
-        "/graphuserwise",
-        "/manageSMPP",
-        "/managerouting",
-        "/SMPPerrorcode",
-        "/manageprefix",
-        "/blacklist",
-        "/managenotifications",
-        "/CreateWhatsappTemplateAdmin",
-        "/manageadduser",
-      ],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "Manage Contacts",
-      links: "/managecontacts",
-      roles: ["ADMIN", "DIRECTUSER"],
-    },
-    {
-      name: "Wish Management",
-      links: "/smswishmanagement",
-      roles: ["ADMIN"],
-    },
-    {
-      name: "API Docs",
-      links: "/docs",
-      roles: ["ADMIN"],
-    },
-    {
-      name: "openRoutes",
-      links: [
-        "/download",
-        "/loginIpdetails",
-        "/profile",
-        "/settings",
-        "/transactions",
-        "/tagmanager",
-      ],
-      roles: ["ADMIN"],
-    },
-    {
-      name: "Download",
-      links: "/dummy",
-      roles: ["ADMIN"],
-    },
-    {
-      name: "WorkFlow",
-      links: ["/workflow", "/workflow/create", "/workflow/edit"],
-
-      roles: ["ADMIN"],
-    },
-  ];
-
   const currentPath = location.pathname;
 
-  const getPrivateRoute = (menuItems, userState) => {
-    let allowedServices = [];
+  const menuItems = useMemo(
+    () => [
+      { name: "Home", links: ["/"], roles: ["ADMIN"] },
+      {
+        name: "SMS",
+        links: [
+          "/sendsms",
+          "/smsreports",
+          "/smsdlttemplates",
+          "/smscampaigndetaillogs",
+          "/smsAttachmentdetaillog",
+        ],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "Two Way SMS",
+        links: ["/managekeywords", "/twowayreports", "/twowayintegration"],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "RCS",
+        links: [
+          "/sendrcs",
+          "/rcsmanagetemplate",
+          "/rcssuggestionreport",
+          "/rcsdeliverycampaigndetails",
+          "/rcsaddtemplatercs",
+          "/rcsdeliveryreport",
+          "/rcsmanagebot",
+          "/rcslivechats",
+        ],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "WHATSAPP",
+        links: [
+          "/wlaunchcampaign",
+          "/wlivechat",
+          "/wmanagecampaign",
+          "/managetemplate",
+          "/wqrcode",
+          "/wmanagewaba",
+          "/wwhatsappconversation",
+          "/wwhatsappmanageagent",
+          "/wwhatsappbot",
+          "/createwhatsappbot",
+          "/wcampaigndetailsreport",
+          "/createtemplate",
+          "/wlcsetting",
+          "/wwhatsappflows",
+          "/wflowcreation",
+          "/apicampaigninfo",
+          "/wblockuser",
+        ],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "Number Lookup",
+        links: ["/hlrlookup", "/lookupreports"],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "App Authenticator",
+        links: ["/authsettings", "/authreports"],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "E-mail",
+        links: ["/emailtemplate", "/emailreports"],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "OBD",
+        links: [
+          "/obdcreatecampaign",
+          "/obdmanagecampaign",
+          "/obdmanagevoiceclips",
+          "/obdIntegration",
+        ],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "IBD",
+        links: [
+          "/ibdcallhistory",
+          "/ibdmanageexecutive",
+          "/ibdivrflow",
+          "/ibdsettings",
+        ],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "Missed Call",
+        links: ["/missedcallhistory", "/missedcallsettings"],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "Click-2-Call",
+        links: ["/clicktohistory", "/clicktosettings"],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "CallBack",
+        links: ["/callback", "/addcallback", "/editcallback"],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "Manage Funds",
+        links: ["/recharge"],
+        roles: ["ADMIN", "DIRECTUSER"],
+      },
+      {
+        name: "Admin",
+        links: [
+          "/manageuser",
+          "/managedlttemplate",
+          "/rcsmanagebot",
+          "/managevoiceclips",
+          "/manageplan",
+          "/accountmanager",
+          "/graphmain",
+          "/graphuserwise",
+          "/manageSMPP",
+          "/managerouting",
+          "/SMPPerrorcode",
+          "/manageprefix",
+          "/blacklist",
+          "/managenotifications",
+          "/CreateWhatsappTemplateAdmin",
+          "/manageadduser",
+        ],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "Manage Contacts",
+        links: ["/managecontacts"],
+        roles: ["ADMIN", "DIRECTUSER"],
+      },
+      {
+        name: "Wish Management",
+        links: ["/smswishmanagement"],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "API Docs",
+        links: ["/docs"],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "openRoutes",
+        links: [
+          "/download",
+          "/loginIpdetails",
+          "/profile",
+          "/settings",
+          "/transactions",
+          "/tagmanager",
+        ],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "Download",
+        links: ["/dummy"],
+        roles: ["ADMIN"],
+      },
+      {
+        name: "WorkFlow",
+        links: ["/workflow", "/workflow/create", "/workflow/edit"],
+        roles: ["ADMIN"],
+      },
+    ],
+    []
+  );
 
-    if (userState.role === "ADMIN") {
-      return menuItems;
-    }
-    if (userState.role === "AGENT") {
+  const privateRoutes = useMemo(() => {
+    if (!user) return [];
+
+    if (user.role === "ADMIN") return menuItems;
+
+    if (user.role === "AGENT") {
       return [
-        {
-          name: "Home",
-          links: "/",
-          roles: ["AGENT"],
-        },
-        {
-          name: "WhatsApp LiveChat",
-          links: "/wlivechat",
-          roles: ["AGENT"],
-        },
+        { name: "Home", links: ["/"], roles: ["AGENT"] },
+        { name: "WhatsApp LiveChat", links: ["/wlivechat"], roles: ["AGENT"] },
         {
           name: "openRoutes",
           links: ["/profile", "/settings"],
-          roles: ["ADMIN"],
+          roles: ["AGENT"],
         },
       ];
     }
 
-    menuItems.forEach((item) => {
-      // if (item.roles.includes(userState.role)) allowedServices.push(item);
-      userState.services.forEach((service, index) => {
-        if (item.name == service.display_name) {
-          allowedServices.push(item);
-        }
-      });
-      if (item.name === "Home") {
-        allowedServices.push(item);
-      }
-      if (item.name === "apiDocs") {
-        allowedServices.push(item);
-      }
-      if (item.name === "CallBack") {
-        allowedServices.push(item);
-      }
-      if (item.name === "openRoutes") {
-        allowedServices.push(item);
-      }
-      if (item.name === "Manage Contacts") {
-        allowedServices.push(item);
-      }
-      if (item.name === "WorkFlow") {
-        allowedServices.push(item);
-      }
-    });
+    // Default for other roles
+    const alwaysInclude = [
+      "Home",
+      "API Docs",
+      "CallBack",
+      "openRoutes",
+      "Manage Contacts",
+      "WorkFlow",
+    ];
 
-    return allowedServices;
-  };
+    return menuItems.filter(
+      (item) =>
+        alwaysInclude.includes(item.name) ||
+        user.services.some((service) => service.display_name === item.name)
+    );
+  }, [user, menuItems]);
 
-  const privateRoute = getPrivateRoute(menuItems, user);
+  const isPathAllowed = useMemo(() => {
+    return privateRoutes.some((item) =>
+      Array.isArray(item.links)
+        ? item.links.includes(currentPath)
+        : item.links === currentPath
+    );
+  }, [privateRoutes, currentPath]);
 
-  if (!privateRoute.some((item) => item?.links?.includes(currentPath)))
-    return <PageNotFound />;
+  if (isLoading || !user)
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Loader />
+      </div>
+    );
+  if (!isPathAllowed) return <PageNotFound />;
   return <Outlet />;
 };
