30adccb58b4f8a4bd69a66dcfe06fa011bfa7e8b
Arihant Jain <arihantj916@gmail.com>
2025-07-18 11:18:09 +0530

    feat: add loading state in authcontext

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -3,69 +3,75 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState({
-        isAuthenticated: false,
-        role: "",
-        services: [],
-        ttl: 0,
-    });
+  const [user, setUser] = useState({
+    isAuthenticated: false,
+    role: "",
+    services: [],
+    ttl: 0,
+  });
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const storedUser = sessionStorage.getItem("user");
+  useEffect(() => {
+    try {
+      const storedUser = sessionStorage.getItem("user");
 
-        if (storedUser) {
-            try {
-                const parsedUser = JSON.parse(storedUser);
-                setUser(parsedUser);
-            } catch (e) {
-                console.error("Failed to parse user from sessionStorage", e);
-            }
-        } else {
-            setUser({
-                isAuthenticated: false,
-                role: "",
-                services: [],
-                ttl: 0,
-            });
+      if (storedUser) {
+        try {
+          const parsedUser = JSON.parse(storedUser);
+          setUser(parsedUser);
+        } catch (e) {
+          console.error("Failed to parse user from sessionStorage", e);
         }
-    }, []);
-
-    const authLogin = (role, services = [], ttl) => {
+      } else {
         setUser({
-            isAuthenticated: true,
-            role,
-            services,
-            ttl,
+          isAuthenticated: false,
+          role: "",
+          services: [],
+          ttl: 0,
         });
-        sessionStorage.setItem(
-            "user",
-            JSON.stringify({
-                isAuthenticated: true,
-                role,
-                services,
-                ttl,
-            })
-        );
-    };
+      }
+    } catch (e) {
+      console.error("Failed to retrieve user from sessionStorage", e);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
 
-    const authLogout = () => {
-        setUser({
-            isAuthenticated: false,
-            role: "",
-            services: [],
-            ttl: 0,
-        });
-        sessionStorage.removeItem("user");
-    };
-
-    // const value = useMemo(() => ({ user, authLogin, authLogout }), [user]);
-
-    return (
-        <UserContext.Provider value={{ user, authLogin, authLogout }}>
-            {children}
-        </UserContext.Provider>
+  const authLogin = (role, services = [], ttl) => {
+    setUser({
+      isAuthenticated: true,
+      role,
+      services,
+      ttl,
+    });
+    sessionStorage.setItem(
+      "user",
+      JSON.stringify({
+        isAuthenticated: true,
+        role,
+        services,
+        ttl,
+      })
     );
+  };
+
+  const authLogout = () => {
+    setUser({
+      isAuthenticated: false,
+      role: "",
+      services: [],
+      ttl: 0,
+    });
+    sessionStorage.removeItem("user");
+  };
+
+  // const value = useMemo(() => ({ user, authLogin, authLogout }), [user]);
+
+  return (
+    <UserContext.Provider value={{ user, authLogin, authLogout, isLoading }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const useUser = () => useContext(UserContext);
-
