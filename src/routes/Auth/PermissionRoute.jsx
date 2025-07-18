f0697235622ae2829e05335f58c9b651b7a15ab6
Arihant Jain <arihantj916@gmail.com>
2025-07-18 11:07:50 +0530

    feat: optimize permissionRoutes

--- src/routes/Auth/PermissionRoute.jsx
+++ src/routes/Auth/PermissionRoute.jsx
@@ -1,68 +1,112 @@
+// import { useUser } from "@/context/auth";
+// import { useLocation } from "react-router-dom";
+// import { Navigate, Outlet } from "react-router-dom";
+// import PageNotFound from "@/NotFound/PageNotFound";
+// import { userItems } from "./routes/user";
+// import { resellerItems } from "./routes/reseller";
+// import { it } from "date-fns/locale";
+
+// export const PermissionRoute = ({ children }) => {
+//   const { user } = useUser();
+//   const location = useLocation();
+
+//   const menuItems = resellerItems;
+
+//   const currentPath = location.pathname;
+
+//   const getPrivateRoute = (menuItems, userState) => {
+//     // if (userState.role === "AGENT") {
+//     //   return [
+//     //     { name: "Home", links: "/", roles: ["ADMIN"] },
+//     //     { name: "WhatsApp Live Chat", links: "/wlivechat" },
+//     //     { name: "WhatsApp Live Chat", links: ["/profile"] },
+//     //   ];
+//     // }
+//     // if (userState.role === "ADMIN") {
+//     //   return menuItems;
+//     // }
+
+//     // const alwaysIncludeNames = [
+//     //   "Home",
+//     //   "apiDocs",
+//     //   "CallBack",
+//     //   "Manage Contacts",
+//     //   "openRoutes",
+//     // ];
+
+//     const allowedServices = [];
+//     const data = {
+//       name: "Reports",
+//       links: [],
+//     };
+//     menuItems.map((item) => {
+//       userState.services.forEach((service) => {
+//         if (item.name === "Reports") {
+//           const links = item.links.filter(
+//             (link) => link.id == service.service_type_id
+//           );
+//           data?.links?.push(...links[0].links);
+//         }
+//       });
+
+//       allowedServices.push(item);
+//     });
+
+//     const filteredData = allowedServices.filter(
+//       (item) => item.name !== "Reports"
+//     );
+//     filteredData.push(data);
+
+//     return filteredData;
+//   };
+
+//   const privateRoute = getPrivateRoute(menuItems, user);
+
+//   if (!privateRoute.some((item) => item?.links.includes(currentPath)))
+//     return <PageNotFound />;
+//   return <Outlet />;
+// };
+
 import { useUser } from "@/context/auth";
-import { useLocation } from "react-router-dom";
-import { Navigate, Outlet } from "react-router-dom";
+import { useLocation, Outlet } from "react-router-dom";
 import PageNotFound from "@/NotFound/PageNotFound";
-import { userItems } from "./routes/user";
 import { resellerItems } from "./routes/reseller";
-import { it } from "date-fns/locale";
+import { useMemo } from "react";
+import Loader from "@/admin/components/Loader";
 
-export const PermissionRoute = ({ children }) => {
-  const { user } = useUser();
+export const PermissionRoute = () => {
+  const { user, loading } = useUser();
   const location = useLocation();
-
-  const menuItems = resellerItems;
-
   const currentPath = location.pathname;
 
-  const getPrivateRoute = (menuItems, userState) => {
-    // if (userState.role === "AGENT") {
-    //   return [
-    //     { name: "Home", links: "/", roles: ["ADMIN"] },
-    //     { name: "WhatsApp Live Chat", links: "/wlivechat" },
-    //     { name: "WhatsApp Live Chat", links: ["/profile"] },
-    //   ];
-    // }
-    // if (userState.role === "ADMIN") {
-    //   return menuItems;
-    // }
+  const privateRoute = useMemo(() => {
+    if (!user) return []; 
 
-    // const alwaysIncludeNames = [
-    //   "Home",
-    //   "apiDocs",
-    //   "CallBack",
-    //   "Manage Contacts",
-    //   "openRoutes",
-    // ];
+    const allowedServices = resellerItems.map((item) => {
+      if (item.name === "Reports") {
+        const allowedLinks = item.links
+          .filter((link) =>
+            user.services.some((service) => link.id === service.service_type_id)
+          )
+          .flatMap((link) => link.links);
 
-    const allowedServices = [];
-    const data = {
-      name: "Reports",
-      links: [],
-    };
-    menuItems.map((item) => {
-      userState.services.forEach((service) => {
-        if (item.name === "Reports") {
-          const links = item.links.filter(
-            (link) => link.id == service.service_type_id
-          );
-          data?.links?.push(...links[0].links);
-        }
-      });
-
-      allowedServices.push(item);
+        return { ...item, links: allowedLinks };
+      }
+      return item;
     });
 
-    const filteredData = allowedServices.filter(
-      (item) => item.name !== "Reports"
+    return allowedServices;
+  }, [user]);
+
+  if (loading)
+    return (
+      <div className="flex items-center justify-center h-[80vh]">
+        <Loader />
+      </div>
     );
-    filteredData.push(data);
 
-    return filteredData;
-  };
-
-  const privateRoute = getPrivateRoute(menuItems, user);
-
-  if (!privateRoute.some((item) => item?.links.includes(currentPath)))
+  if (!privateRoute.some((item) => item?.links?.includes(currentPath)))
     return <PageNotFound />;
+
   return <Outlet />;
 };
