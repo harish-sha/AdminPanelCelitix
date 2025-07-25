a9c045a2f4757fbf8b83952e25d770c08e1ef244
Arihant Jain <arihantj916@gmail.com>
2025-07-25 13:52:11 +0530

    feat: temo commit

--- src/admin/manageNotifications.jsx/components/whatsapp/index.jsx
+++ src/admin/manageNotifications.jsx/components/whatsapp/index.jsx
@@ -1,3 +1,4 @@
+import AnimatedDropdown from "@/admin/components/AnimatedDropdown";
 import {
   getTemplateDetialsById,
   getTemplateList,
@@ -77,5 +78,53 @@ export const Whatsapp = ({ handleSaveWhatsappNotification, data, setData }) => {
     handleGetTemplateDetails();
   }, [templateState.selected]);
 
-  return <div>WhatsApp</div>;
+  return (
+    <div className="grid grid-cols-3">
+      <div className="space-y-2 col-span-2">
+        <div>
+          <AnimatedDropdown
+            id="waba"
+            label="Select WABA"
+            name="waba"
+            options={wabaState.all.map((waba) => ({
+              value: waba.mobileNo,
+              label: waba.name,
+            }))}
+            value={wabaState.selected}
+            onChange={(e) => {
+              setWabaState((prev) => ({
+                ...prev,
+                selected: e,
+              }));
+              setTemplateState({
+                details: {},
+                all: [],
+                selected: "",
+              });
+            }}
+          />
+        </div>
+        <div>
+          <AnimatedDropdown
+            id="template"
+            name="template"
+            label="Select Template"
+            options={templateState.all.map((template) => ({
+              value: template.vendorTemplateId,
+              label: template.templateName,
+            }))}
+            value={templateState.selected}
+            onChange={(e) => {
+              setTemplateState((prev) => ({
+                ...prev,
+                selected: e,
+              }));
+            }}
+          />
+        </div>
+      </div>
+
+      <div className="w-full p-2">Preview</div>
+    </div>
+  );
 };
