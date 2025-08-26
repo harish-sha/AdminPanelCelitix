c8481698eeb6fbe50e9de4c1d14ae15de10c8bd5
Arihant Jain <arihantj916@gmail.com>
2025-08-26 12:17:52 +0530

    feat: add api to fetch data delete the error code

--- src/admin/smmpErrorCode/SMPPErrorCode.jsx
+++ src/admin/smmpErrorCode/SMPPErrorCode.jsx
@@ -1,220 +1,407 @@
-import React, { useState } from 'react'
-import UniversalButton from '../../whatsapp/components/UniversalButton'
-import DropdownWithSearch from '../../whatsapp/components/DropdownWithSearch';
-import SMPPErrorCodeTable from './components/SMPPErrorCodeTable';
-import UniversalSkeleton from '../../whatsapp/components/UniversalSkeleton';
-import AnimatedDropdown from '../../whatsapp/components/AnimatedDropdown';
-import InputField from '../../whatsapp/components/InputField';
-import UniversalTextArea from '../../whatsapp/components/UniversalTextArea';
-import { Dialog } from 'primereact/dialog';
+import React, { useEffect, useState } from "react";
+import UniversalButton from "../../whatsapp/components/UniversalButton";
+import DropdownWithSearch from "../../whatsapp/components/DropdownWithSearch";
+import SMPPErrorCodeTable from "./components/SMPPErrorCodeTable";
+import UniversalSkeleton from "../../whatsapp/components/UniversalSkeleton";
+import AnimatedDropdown from "../../whatsapp/components/AnimatedDropdown";
+import InputField from "../../whatsapp/components/InputField";
+import UniversalTextArea from "../../whatsapp/components/UniversalTextArea";
+import { Dialog } from "primereact/dialog";
+import toast from "react-hot-toast";
+import {
+  deleteErrCode,
+  getAppErrorReasons,
+  getErrorMappingData,
+  getMissingErrorCodeData,
+  getServices,
+} from "@/apis/admin/admin";
+import CustomTooltip from "@/components/common/CustomTooltip";
+import { IconButton } from "@mui/material";
+import { MdOutlineDeleteForever } from "react-icons/md";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import { DataTable } from "@/components/layout/DataTable";
+import { Delete } from "lucide-react";
 
 const SMPPErrorCode = () => {
   const [isFetching, setIsFetching] = useState(false);
-  const [smpperrorcodeadd, setSMPPErrorCodeAdd] = useState(false);
+  const [searchData, setSearchData] = useState({
+    serviceId: "",
+    error: "",
+  });
+  const [rows, setRows] = useState([]);
+  const [data, setData] = useState({
+    service: [],
+    error: [],
+  });
+  const [deleteData, setDeleteData] = useState({
+    isOpen: false,
+    id: null,
+  });
 
-  const handleSMPPErrorAdd = () => {
-    setSMPPErrorCodeAdd(true);
-  };
+  useEffect(() => {
+    async function handleGetServices() {
+      try {
+        const res = await getServices();
+        if (!res?.status) {
+          return toast.error("Something went wrong");
+        }
+        setData((prev) => ({
+          ...prev,
+          service: res?.data,
+        }));
+      } catch (e) {
+        console.log("e", e);
+        return toast.error("Something went wrong");
+      }
+    }
 
-  const serviceOptionsadd = [
-    { value: 'Service1', label: 'Service1' },
-    { value: 'Service2', label: 'Service2' },
-    { value: 'Service3', label: 'Service3' },
-  ];
-  const handleServiceAdd = (service) => {
-    // console.log(service);
-  };
-  const displaytypeOptions = [
-    { value: 'Display1', label: 'Display1' },
-    { value: 'Display2', label: 'Display2' },
-    { value: 'Display3', label: 'Display3' },
-  ];
-  const handleDisplaytyperAdd = (display) => {
-    // console.log(display);
-  };
-  const displayreasonOptions = [
-    { value: 'Vendor1', label: 'Vendor1' },
-    { value: 'Vendor2', label: 'Vendor2' },
-    { value: 'Vendor3', label: 'Vendor3' },
-  ];
-  const handleDisplayreasonAdd = (reason) => {
-    // console.log(reason);
-  };
+    async function handleGetErrorCode() {
+      try {
+        const res = await getAppErrorReasons();
+        if (!res?.status) {
+          return toast.error("Something went wrong");
+        }
+        setData((prev) => ({
+          ...prev,
+          error: res?.data,
+        }));
+      } catch (e) {
+        console.log("e", e);
+        return toast.error("Something went wrong");
+      }
+    }
 
+    handleGetServices();
+    handleGetErrorCode();
+  }, []);
 
-  const serviceOptions = [
-    { label: 'Service 1', value: 'service1' },
-    { label: 'Service 2', value: 'service2' },
-    { label: 'Service 3', value: 'service3' },
-    { label: 'Service 4', value: 'service4' },
-  ];
+  async function handleSearch() {
+    if (!searchData?.serviceId) return;
+    try {
+      setIsFetching(true);
+      const res = await getErrorMappingData(searchData?.serviceId);
+      if (!res?.status) {
+        return toast.error("Something went wrong");
+      }
+      const filteredData = searchData?.error
+        ? res?.data?.filter(
+            (data) => data?.display_err_code === searchData?.error
+          )
+        : res?.data;
+      const getSeviceName = data?.service?.find(
+        (item) => item?.serviceid === searchData?.serviceId
+      )?.servicename;
 
-  const errorCodeOptions = [
-    { label: 'Error Code 1', value: 'errorcode1' },
-    { label: 'Error Code 2', value: 'errorcode2' },
-    { label: 'Error Code 3', value: 'errorcode3' },
-    { label: 'Error Code 4', value: 'errorcode4' },
-  ];
-  return (
-    <div className='w-full'>
-      {/* {isLoading ? (
-    <>
-      <Loader />
-    </>
-  ) : ( */}
-      <div>
+      const formattedData = Array.isArray(filteredData)
+        ? filteredData.map((item, index) => ({
+            id: index + 1,
+            sn: index + 1,
+            ...item,
+          }))
+        : [];
+      setRows(formattedData);
+    } catch (e) {
+      return toast.error("Something went wrong");
+    } finally {
+      setIsFetching(false);
+    }
+  }
 
-        <div className="flex flex-wrap gap-2 items-end justify-between pb-3 w-full">
-          <div className='flex flex-wrap gap-2 items-end'>
-            <div className="w-max-content">
-              <DropdownWithSearch
-                label="Service"
-                options={serviceOptions}
-                placeholder="Select Service"
-                id="smpperrorservice"
-                name="smpperrorservice"
-              // onChange={handleerrorCodeServiceChange}
+  const columns = [
+    { field: "sn", headerName: "S.No", flex: 0, minWidth: 50 },
+    {
+      field: "vendor_error_status",
+      headerName: "Vendor EC Status",
+      flex: 1,
+      minWidth: 120,
+    },
+    {
+      field: "vendor_error_code",
+      headerName: "Vendor EC",
+      flex: 1,
+      minWidth: 90,
+    },
+    {
+      field: "vendor_error_code_description",
+      headerName: "Vendor EC Description",
+      flex: 1,
+      minWidth: 120,
+    },
+    {
+      field: "display_reason",
+      headerName: "Display EC Status",
+      flex: 1,
+      minWidth: 120,
+    },
+    {
+      field: "display_err_code",
+      headerName: "Display EC",
+      flex: 1,
+      minWidth: 100,
+    },
+
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      minWidth: 100,
+      renderCell: (params) => (
+        <>
+          <CustomTooltip title="Edit Routing" placement="top" arrow>
+            <IconButton onClick={() => {}}>
+              <EditNoteIcon
+                sx={{
+                  fontSize: "1.2rem",
+                  color: "gray",
+                }}
               />
-            </div>
-            <div className="w-max-content">
+            </IconButton>
+          </CustomTooltip>
+          <CustomTooltip title="Delete Routing" placement="top" arrow>
+            <IconButton
+              className="no-xs"
+              onClick={() => {
+                setDeleteData({
+                  isOpen: true,
+                  id: params.row.sr_no,
+                });
+              }}
+            >
+              <MdOutlineDeleteForever
+                className="text-red-500 cursor-pointer hover:text-red-600"
+                size={20}
+              />
+            </IconButton>
+          </CustomTooltip>
+        </>
+      ),
+    },
+  ];
+
+  async function handleDelete() {
+    try {
+      if (!deleteData?.id) return;
+      const res = await deleteErrCode(deleteData?.id);
+
+      if (!res?.status) {
+        return toast.error("Something went wrong");
+      }
+      toast.success("Deleted successfully");
+      setDeleteData({ isOpen: false, id: null });
+      handleSearch();
+    } catch (e) {
+      toast.error("Something went wrong");
+    }
+  }
+
+  return (
+    <div>
+      <div className="flex flex-wrap gap-2 items-end justify-between pb-3 w-full">
+        <div className="flex gap-2 items-end  w-full">
+          <div className="flex gap-2">
+            <DropdownWithSearch
+              label="Service"
+              options={data?.service?.map((data) => ({
+                label: data?.servicename,
+                value: data?.serviceid,
+              }))}
+              placeholder="Select Service"
+              id="smpperrorservice"
+              name="smpperrorservice"
+              value={searchData?.serviceId}
+              onChange={(e) =>
+                setSearchData((prev) => ({ ...prev, serviceId: e }))
+              }
+            />
+            <div className="w-[20rem]">
               <DropdownWithSearch
                 label="Error Code"
-                options={errorCodeOptions}
+                options={data?.error?.map((data) => ({
+                  label: data?.error_reason,
+                  value: data?.error_code,
+                }))}
                 placeholder="Select Error Code"
                 id="smpperrorcode"
                 name="smpperrorcode"
-              // onChange={handleErrorCodeChange}
-              />
-            </div>
-            <div className="w-max-content">
-              <UniversalButton
-                label="Search"
-                id="searcherrorcode"
-                name="searcherrorcode"
-              // onClick={handleSearchErrorCode}
-              />
-            </div>
-            <div className="w-max-content">
-              <UniversalButton
-                label="Delete"
-                id="deleteerrorcode"
-                name="deleteerrorcode"
-              // onClick={handleDeleteErrorCode}
+                value={searchData?.error}
+                onChange={(e) =>
+                  setSearchData((prev) => ({ ...prev, error: e }))
+                }
               />
             </div>
           </div>
+
           <div className="w-max-content">
             <UniversalButton
-              label="Add Error Code"
-              id="adderrorcode"
-              name="adderrorcode"
-              onClick={handleSMPPErrorAdd}
+              label="Search"
+              id="searcherrorcode"
+              name="searcherrorcode"
+              onClick={handleSearch}
             />
+          </div>
+          {/* <div className="w-max-content">
+            <UniversalButton
+              label="Delete"
+              id="deleteerrorcode"
+              name="deleteerrorcode"
+              // onClick={handleDeleteErrorCode}
+            />
+          </div> */}
+        </div>
+        <div className="w-max-content">
+          <UniversalButton
+            label="Add Error Code"
+            id="adderrorcode"
+            name="adderrorcode"
+            onClick={() => {}}
+          />
+        </div>
+      </div>
 
+      {/* ✅ Show Loader or Table */}
+      {isFetching ? (
+        <div className="w-full">
+          <UniversalSkeleton height="35rem" width="100%" />
+        </div>
+      ) : (
+        <DataTable
+          id="smpperrorcode"
+          name="smpperrorcode"
+          col={columns}
+          rows={rows}
+        />
+      )}
+
+      <Dialog
+        header="Vendor Error Code Mapping"
+        visible={false}
+        onHide={() => {}}
+        className="lg:w-[40rem] md:w-[30rem] w-[20rem]"
+        draggable={false}
+      >
+        <div className="space-y-4">
+          <div className="grid grid-cols-2 gap-4">
+            <AnimatedDropdown
+              label="Service"
+              options={[{}]}
+              id="serviceadd"
+              name="serviceadd"
+              onChange={() => {}}
+            />
+            <InputField
+              label="Vendor Error Code"
+              id="vendorerrorcodeadd"
+              name="vendorerrorcodeadd"
+              placeholder="Enter Vendor Error Code"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <InputField
+              label="Vendor Error Status"
+              id="vendorerrorstatusadd"
+              name="vendorerrorstatusadd"
+              placeholder="Vendor Error Status"
+            />
+            <UniversalTextArea
+              label="Vendor Error Code Description"
+              id="vendorerrorcodedescriptionadd"
+              name="vendorerrorcodedescriptionadd"
+              placeholder="Vendor Error Code Description"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <AnimatedDropdown
+              label="Display Type"
+              options={[{}]}
+              id="displaytypeadd"
+              name="displaytypeadd"
+              onChange={() => {}}
+            />
+            <AnimatedDropdown
+              label="Display Reason"
+              options={[{}]}
+              id="displayreasonadd"
+              name="displayreasonadd"
+              onChange={() => {}}
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <InputField
+              label="Display Error Code"
+              id="displayerrorcodeadd"
+              name="displayerrorcodeadd"
+              placeholder="Display Error Code"
+              readOnly="true"
+            />
+          </div>
+          <div className="">
+            <label className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                name="refundOnDelivery"
+                className="form-checkbox"
+              />
+              <span className="text-sm">Refund For On Delivery Account</span>
+            </label>
+            <label className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                name="dndRefund"
+                className="form-checkbox"
+              />
+              <span className="text-sm">DND Refund</span>
+            </label>
+          </div>
+
+          <div className="flex justify-center">
+            <UniversalButton label="Save" id="saveadd" name="saveadd" />
           </div>
         </div>
+      </Dialog>
 
-        {/* ✅ Show Loader or Table */}
-        {isFetching ? (
-          <div className="w-full">
-            <UniversalSkeleton height="35rem" width="100%" />
-          </div>
-        ) : (
-          <div className="w-full">
-            <SMPPErrorCodeTable
-              id="SMPPErrorCodeTable"
-              name="SMPPErrorCodeTable"
-
-            />
-          </div>
-        )}
-
-        <Dialog
-          header="Vendor Error Code Mapping"
-          visible={smpperrorcodeadd}
-          onHide={() => setSMPPErrorCodeAdd(false)}
-          className="lg:w-[40rem] md:w-[30rem] w-[20rem]"
-          draggable={false}
-        >
-          <div className='space-y-4'>
-            <div className='grid grid-cols-2 gap-4'>
-              <AnimatedDropdown
-                label="Service"
-                options={serviceOptionsadd}
-                id="serviceadd"
-                name="serviceadd"
-                onChange={handleServiceAdd}
-              />
-              <InputField
-                label="Vendor Error Code"
-                id="vendorerrorcodeadd"
-                name="vendorerrorcodeadd"
-                placeholder='Enter Vendor Error Code'
-              />
-            </div>
-            <div className='grid grid-cols-2 gap-4'>
-              <InputField
-                label="Vendor Error Status"
-                id="vendorerrorstatusadd"
-                name="vendorerrorstatusadd"
-                placeholder="Vendor Error Status"
-              />
-              <UniversalTextArea
-                label="Vendor Error Code Description"
-                id="vendorerrorcodedescriptionadd"
-                name="vendorerrorcodedescriptionadd"
-                placeholder="Vendor Error Code Description"
-
-              />
-            </div>
-            <div className='grid grid-cols-2 gap-4'>
-              <AnimatedDropdown
-                label="Display Type"
-                options={displaytypeOptions}
-                id="displaytypeadd"
-                name="displaytypeadd"
-                onChange={handleDisplaytyperAdd}
-              />
-              <AnimatedDropdown
-                label="Display Reason"
-                options={displayreasonOptions}
-                id="displayreasonadd"
-                name="displayreasonadd"
-                onChange={handleDisplayreasonAdd}
-              />
-            </div>
-            <div className='grid grid-cols-2 gap-4'>
-              <InputField
-                label="Display Error Code"
-                id="displayerrorcodeadd"
-                name="displayerrorcodeadd"
-                placeholder="Display Error Code"
-                readOnly="true"
-              />
-            </div>
-            <div className="">
-              <label className="flex items-center space-x-2">
-                <input type="checkbox" name="refundOnDelivery" className="form-checkbox" />
-                <span className='text-sm'>Refund For On Delivery Account</span>
-              </label>
-              <label className="flex items-center space-x-2">
-                <input type="checkbox" name="dndRefund" className="form-checkbox" />
-                <span className='text-sm'>DND Refund</span>
-              </label>
-            </div>
-
-            <div className='flex justify-center'>
+      <Dialog
+        header="Confirm Delete"
+        visible={deleteData.isOpen}
+        onHide={() => {
+          setDeleteData((prev) => ({ isOpen: false, id: "" }));
+        }}
+        className="lg:w-[40rem] md:w-[30rem] w-[20rem]"
+        draggable={false}
+      >
+        <div>
+          <div className="p-4 text-center">
+            <p className="text-[1.1rem] font-semibold text-gray-600">
+              Are you sure ?
+            </p>
+            <p>
+              Do you really want to delete this? This process cannot be undo.
+            </p>
+            <div className="flex justify-center gap-4 mt-2">
               <UniversalButton
-                label="Save"
-                id="saveadd"
-                name="saveadd"
+                label="Cancel"
+                style={{
+                  backgroundColor: "#090909",
+                }}
+                onClick={() => {
+                  setDeleteData((prev) => ({ isOpen: false, id: "" }));
+                }}
+              />
+              <UniversalButton
+                label="Delete"
+                variant="danger"
+                style={
+                  {
+                    // backgroundColor: "red",
+                  }
+                }
+                onClick={() => handleDelete()}
               />
             </div>
           </div>
-        </Dialog>
-      </div>
+        </div>
+      </Dialog>
     </div>
-  )
-}
+  );
+};
 
-export default SMPPErrorCode
+export default SMPPErrorCode;
