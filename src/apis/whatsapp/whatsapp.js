6941dfbb1946ddd6a3ea0325d8b08686de04a1a5
Arihant Jain <arihantj916@gmail.com>
2025-08-26 14:18:14 +0530

    feat: add unread key in readMessage

--- src/apis/whatsapp/whatsapp.js
+++ src/apis/whatsapp/whatsapp.js
@@ -406,7 +406,7 @@ export const downloadAttachment = async (data) => {
 // read message (live chat)
 export const readMessage = async (data) => {
   return await fetchWithAuth(
-    `/LiveChat/isviewChat?srno=${data.srno}&wabaNumber=${data.waba}&mobile=${data.mobile}`,
+    `/LiveChat/isviewChat?srno=${data.srno}&wabaNumber=${data.waba}&mobile=${data.mobile}&unreadFlag=${data.unreadFlag}`,
     {
       method: "POST",
     }
